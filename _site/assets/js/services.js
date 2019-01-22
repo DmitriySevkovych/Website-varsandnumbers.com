6a8c44da4dd3de372d23f333d22da9a15edc1367
Dmitriy Sevkovych <d.sevkovych@web.de>
2019-01-22 18:18:40 +0100

    Refactor jQuery on services page. Add jQueryUI

--- _site/assets/js/services.js
+++ _site/assets/js/services.js
@@ -2,7 +2,7 @@ $(function() {
   var $services = $('li');
   var $scrollContainer = $('.scrollable');
 
-  var scrollDelta = 100;
+  var scrollDelta = 200;
   var scrollPosition = -1;
   var lastScrollTop = scrollPosition * scrollDelta;
 
@@ -17,31 +17,40 @@ $(function() {
     } else {
       scrollPosition--;
     }
+
+    if (scrollPosition < -1) {
+      scrollPosition = -1;
+    } else if (scrollPosition > 9) {
+      scrollPosition = 9;
+    }
+
     lastScrollTop = st;
 
     updateScrollPrompt(scrollPosition);
     $.each($services, function() {
-        updateServices(scrollPosition, $(this));
+      updateService(scrollPosition, $(this));
     });
     updateRoundup(scrollPosition);
-
-
   });
 });
 
 
 function updateScrollPrompt(scrollPosition) {
-  var $scrollPrompt = $('#scroll-prompt');
-  if (scrollPosition < 1) {
-    // $scrollPrompt.show(300);
+  var $scrollPrompt = $('.scroll-prompt');
+  if (scrollPosition < 1 || scrollPosition > 7) {
+    $scrollPrompt.css({
+      opacity: 1
+    });
   } else {
-    // $scrollPrompt.hide(300);
+    $scrollPrompt.css({
+      opacity: 0
+    });
   }
 }
 
 
-function updateServices(scrollPosition, service) {
-  var index = service.attr('data-index');
+function updateService(scrollPosition, service) {
+  const index = service.attr('data-index');
 
   service.removeClass();
 
@@ -49,21 +58,46 @@ function updateServices(scrollPosition, service) {
     service.addClass('focused');
   } else if (index == scrollPosition - 1 || index == scrollPosition + 1) {
     service.addClass('fading');
-  } else if (index < scrollPosition - 1) {
+  } else if (index == scrollPosition - 2) {
     service.addClass('faded');
   } else {
     service.addClass('yet-to-show');
   }
+  positionService(scrollPosition, service);
 }
+
+
+function positionService(scrollPosition, service) {
+
+  const y = $('.graphics').height() / 2;
+  const index = service.attr('data-index');
+  var positionString;
+
+  if (index == scrollPosition) {
+    positionString = 'center center';
+  } else if (index == scrollPosition + 1) {
+    positionString = 'center center+' + y;
+  } else if (index == scrollPosition - 1) {
+    positionString = 'center center-' + y;
+  } else if (index < scrollPosition - 1) {
+    positionString = 'center center-' + 2*y;
+  }
+
+  service.position({
+    my: 'center center',
+    at: positionString,
+    of: '.get-attention'
+  });
+}
+
 
 function updateRoundup(scrollPosition) {
   var $roundup = $('.round-up');
 
-  if (scrollPosition > 8) {
+  if (scrollPosition == 9) {
     $roundup.removeClass('yet-to-show');
-    $roundup.addClass('focused');
+    $roundup.slideDown(1000)
   } else {
-    $roundup.removeClass('focused');
-    $roundup.addClass('yet-to-show');
+    $roundup.slideUp(1000)
   }
 }
