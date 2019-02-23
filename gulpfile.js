6da9fc31ff6bcdf4984e6b06e7239104811690b3
Dmitriy Sevkovych <d.sevkovych@web.de>
2019-02-23 23:26:04 +0100

    Configure Gulp task for bundling external libraries to ext.min.js

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+/* Gulp and plugins */
+var gulp = require('gulp');
+// var sourcemaps = require('gulp-sourcemaps');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+
+/* External libraries to be bundled */
+var bootstrapMinJs = './node_modules/bootstrap/dist/js/bootstrap.min.js';
+var jqueryMinJs = './node_modules/jquery/dist/jquery.min.js';
+var jqueryUiMinJs = './assets/js/jquery-ui.min.js';
+var touchswipeMinJs = './node_modules/jquery-touchswipe/jquery.touchSwipe.min.js';
+var slickMinJs = './node_modules/slick-carousel/slick/slick.min.js';
+
+var extLibs = [jqueryMinJs, bootstrapMinJs, touchswipeMinJs, slickMinJs];
+
+
+/* Gulp tasks */
+gulp.task('get-ext-js', function() {
+  return gulp.src(extLibs)
+    // .pipe(sourcemaps.init())
+    .pipe(concat('ext.js'))
+    .pipe(uglify())
+    // .pipe(sourcemaps.write())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('./assets/js'));
+});
