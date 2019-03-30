1e298ddc96d25186f4241518812a231e011fc740
Dmitriy Sevkovych <d.sevkovych@web.de>
2019-03-30 15:33:26 +0100

    Refactor expertise: Update intro text positioning. Disable popovers (propaply thay will be removed altogether), add svg popups instead (just a test so far).

--- assets/js/expertise.js
+++ assets/js/expertise.js
@@ -9,15 +9,25 @@ $(function() {
       $navBlend.fadeToggle(300);
   });
 
+  // console.log($("#expFrameMaths").position());
+  // console.log($("#expFrameMaths").width());
+  // console.log($("#expFrameMaths").height());
 
-  $.get('./assets/popovers/popover_expSimulation.html', function(markup){
-    $('[data-toggle="popover"]').popover({
-      html : true,
-      content : markup
-    });
-  });
+  // $.get('./assets/popovers/popover_expSimulation.html', function(markup){
+  //   $('[data-toggle="popover"]').popover({
+  //     html : true,
+  //     content : markup
+  //   });
+  // });
 
 
 
   // $('[data-toggle="popover"]').popover();
+  $('[data-toggle="popover"]').on('click', function(){
+    $('.expertise-popup').fadeIn(300);
+  });
+
+  $('.expertise-popup').on('click', function(){
+    $('.expertise-popup').fadeOut(300);
+  });
 });
