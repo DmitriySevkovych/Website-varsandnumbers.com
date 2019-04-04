79a5a609950ecbfecb513a81f2e8446cabb6b39f
Dmitriy Sevkovych <d.sevkovych@web.de>
2019-04-04 21:59:53 +0200

    Rename Expertise in Skills

--- assets/js/skills.js
+++ assets/js/skills.js
@@ -9,13 +9,13 @@ $(function() {
       $navBlend.fadeToggle(300);
   });
 
-  $('[data-toggle="popover"]').on('click', function(){
+  $('[data-toggle="popup"]').on('click', function(){
     $popup = $('[data-popup="' + this.id + '"]');
     $popup.fadeIn(400);
     $popup.addClass('popup-open');
   });
 
-  $('.expertise-popup').on('click', function(){
+  $('.skills-popup').on('click', function(){
     $popup = $('.popup-open');
     $popup.fadeOut(400);
     $popup.removeClass('popup-open');
