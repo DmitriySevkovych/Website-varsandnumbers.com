d1491582143d2bc2414bf0af573b7a889b3a5bd3
Dmitriy Sevkovych <d.sevkovych@web.de>
2019-05-15 00:43:34 +0200

    Redesign contact.

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,9 +1,7 @@
-var contact = document.getElementById('mBig');
-contact.innerHTML = 'info' + '@' + '<span>vars</span>and<span>numbers</span>' + '.' + 'com';
+var mailto = document.getElementById('mailto');
+mailto.innerHTML = 'info' + '@' + '<span>vars</span>and<span>numbers</span>' + '.' + 'com';
+mailto.setAttribute('href', 'mailto:' + 'info' + '@' + 'varsandnumbers' + '.' + 'com');
 
-var contactform = document.getElementById('formaction');
-// contactform.setAttribute('action', '//formspree.io/' + 'd.sevkovych' + '@' + 'varsandnumbers' + '.' + 'com');
-contactform.setAttribute('action', '/assets/cgi/test.php');
 
 /*
 Document ready function
