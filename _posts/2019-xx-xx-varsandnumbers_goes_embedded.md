42cc94720e4e530fc453131db460c4ec5e96e228
Dmitriy Sevkovych <d.sevkovych@web.de>
2019-05-14 21:23:43 +0200

    Update keyframes. Mess around with CGI.

---
+++ _posts/2019-xx-xx-varsandnumbers_goes_embedded.md
@@ -0,0 +1,10 @@
+---
+layout: post
+title: "Vars&Numbers goes Embedded!"
+author: "Dmitriy Sevkovych"
+categories: [milestone,embedded]
+tags: [embedded,C,C++,arduino]
+ref: blog
+discussable: true
+published: false
+---
--- assets/js/contact.js
+++ assets/js/contact.js
@@ -2,8 +2,8 @@ var contact = document.getElementById('mBig');
 contact.innerHTML = 'info' + '@' + '<span>vars</span>and<span>numbers</span>' + '.' + 'com';
 
 var contactform = document.getElementById('formaction');
-contactform.setAttribute('action', '//formspree.io/' + 'd.sevkovych' + '@' + 'varsandnumbers' + '.' + 'com');
-
+// contactform.setAttribute('action', '//formspree.io/' + 'd.sevkovych' + '@' + 'varsandnumbers' + '.' + 'com');
+contactform.setAttribute('action', '/assets/cgi/test.php');
 
 /*
 Document ready function
