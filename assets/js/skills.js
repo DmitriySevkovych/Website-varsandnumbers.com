f3e9e7fb71424bda02a82b3f9ee6b433c54f54ac
Dmitriy Sevkovych <d.sevkovych@web.de>
2019-05-19 19:49:02 +0200

    Add vivus to mobile page. Add vivus to german SVGs.

--- assets/js/skills.js
+++ assets/js/skills.js
@@ -3,9 +3,28 @@ Document ready function
 */
 $(function() {
   // Vivus: SVG animation
-  var vivus = new Vivus('vivusAll', {
-    animTimingFunction: Vivus.EASE_OUT
-  });
+  var $sm = $(window).width() < 768;
+  if ($sm) {
+    useVivusOn('svgSkillsSoft', true, function() {
+      useVivusOn('svgSkillsMaths', false, function() {
+        useVivusOn('svgSkillsBoth', false, function() {});
+      });
+    });
+  } else {
+    useVivusOn('svgSkillsAll', false, function() {});
+  }
+
+  function useVivusOn(svgId, autostart, callback) {
+    var start = autostart ? 'autostart' : 'inViewport';
+
+    new Vivus(svgId, {
+        animTimingFunction: Vivus.EASE_OUT,
+        start: start,
+        forceRender: true
+      },
+      callback());
+  }
+
 
   // Data toggle functionality
   $('[data-toggle="popup"]').on('click', function() {
